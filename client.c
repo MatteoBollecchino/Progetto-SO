3a099fac0fb7e9600b7be296ec77a07ea662144d
MatteoBollecchino <116268391+MatteoBollecchino@users.noreply.github.com>
2024-08-06 12:29:58 +0200

    Add files via upload

---
+++ client.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <signal.h> 
+
+#include "funzioniClient.h"
+
+int main(int argc, char *argv[]){
+
+    /*void (*oldHandlerInt) (int); 
+    oldHandlerInt = signal(SIGINT,SIG_IGN);*/
+
+    int clientFd, dimServer, risultato;
+
+    struct sockaddr_in indirizzoINETServer;
+    struct sockaddr* indirizzoINETServerPtr;
+
+    // Creazione Socket del Client
+    clientFd = socket(AF_INET, SOCK_STREAM, DEFAULT_PROTOCOL);
+    if(clientFd < 0){
+        generazioneErrore("Creazione Socket del Client Fallita \n");
+    }
+
+    indirizzoINETServerPtr = (struct sockaddr*) &indirizzoINETServer;
+    dimServer = sizeof(indirizzoINETServer);
+
+    indirizzoINETServer.sin_family = AF_INET;
+    indirizzoINETServer.sin_port = htons(PORTA);
+    indirizzoINETServer.sin_addr.s_addr =  htonl(INADDR_LOOPBACK);
+
+    // Connessione con il Server
+    printf("Tentata Connesione con Server \n");
+
+    do{
+        risultato = connect(clientFd,indirizzoINETServerPtr,dimServer);
+        if(risultato == -1){
+            sleep(1);
+        }
+        printf("Connessione con Sever Fallita \n");
+    }while(risultato == -1);
+
+    printf("Connessione con Server andata a buon fine \n");
+
+    // Fase in cui il client determina l'operazione da richiedere la server
+
+    int richiesta = atoi(argv[1]);
+    if(richiesta < 1 || richiesta >7){
+        generazioneErrore("Codice richiesta non valido \n");
+    }
+
+    // Fase in cui il client inoltra, tramite socket, il codice della richiesta al server
+
+    int scritto = send(clientFd,&richiesta,sizeof(richiesta),0);
+    if(scritto < 0){
+        generazioneErrore("Scrittura su socket fallita \n");
+    }
+
+    // Fase in cui il client riceve l'eventuale richiesta di password
+
+    // Chiusura della connessione con il Server
+    close(clientFd);
+
+    return 0;
+}
